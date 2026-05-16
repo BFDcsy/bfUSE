73f0fafa45ff58c14e27399221c8c46c4c343a4d
BFDcsy <139471144+BFDcsy@users.noreply.github.com>
2026-05-16 15:44:41 +0800

    Add detailed diet guidance popups

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fitness-checkin-pwa-v1";
+const CACHE_NAME = "fitness-checkin-pwa-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.svg", "./icon-512.svg"];
 
 self.addEventListener("install", event => {
