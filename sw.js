a29cea683c57eb028d673bf987930ad85f459356
BFDcsy <139471144+BFDcsy@users.noreply.github.com>
2026-05-19 10:17:22 +0800

    Add daily nutrition estimator

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fitness-checkin-pwa-v3";
+const CACHE_NAME = "fitness-checkin-pwa-v4";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.svg", "./icon-512.svg"];
 
 self.addEventListener("install", event => {
